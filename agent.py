7fe7d9f777b52351f84cb6b63b4c7c66ce19d90a
kora11e <87006301+kora11e@users.noreply.github.com>
2024-06-12 21:29:42 +0200

    update

---
+++ agent.py
@@ -0,0 +1,106 @@
+import torch
+import random
+import numpy as np
+from snake_game import SnakeGameAI, Direction, Point
+from collections import deque
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0
+        self.memory = deque(maxlen=MAX_MEMORY)
+        self.model = None
+        self.trainer = None
+
+    def getState(self, game):
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y + 20)
+        point_d = Point(head.x, head.y - 20)
+
+        dir_l = game.direction == Direction.RIGHT
+        dir_r = game.direction == Direction.LEFT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            (dir_r and game.is_collision(point_r)) or 
+            (dir_l and game.is_collision(point_l)) or 
+            (dir_u and game.is_collision(point_u)) or 
+            (dir_d and game.is_collision(point_d)),
+
+            (dir_u and game.is_collision(point_r)) or 
+            (dir_d and game.is_collision(point_l)) or 
+            (dir_l and game.is_collision(point_u)) or 
+            (dir_r and game.is_collision(point_d)),
+
+            (dir_d and game.is_collision(point_r)) or 
+            (dir_u and game.is_collision(point_l)) or 
+            (dir_r and game.is_collision(point_u)) or 
+            (dir_l and game.is_collision(point_d)),
+
+            dir_l,
+            dir_r,
+            dir_u,
+            dir_d,
+
+            game.food.x < game.head.x,
+            game.food.x > game.head.x,
+            game.food.y < game.head.y,
+            game.food.y > game.head.y
+        
+        ]
+        return np.array(state, dtype=int)
+
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+
+    def get_action(self):
+        pass
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        state_old = agent.getState(game)
+        final_move = agent.get_action(state_old)
+        
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.getState(game)
+
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+
+            print('Game', agent.n_games, 'Score', score, 'Record', record)
+
+if __name__ == '__main__':
+    train()
